076578cf9df87175a27c3b1842eb2992f681a203
ShamzDC <shamsul@dreamcreators.in>
2024-07-04 16:33:28 +0530

    Update Program.cs

--- CHEExportsAPI/Program.cs
+++ CHEExportsAPI/Program.cs
@@ -6,16 +6,17 @@ using Swashbuckle.AspNetCore.SwaggerUI;
 using System.Reflection.PortableExecutable;
 
 var builder = WebApplication.CreateBuilder(args);
- 
 
-builder.Services.AddControllers(); 
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Your API", Version = "v1" });
 
-
+    // Include the JWT token in the Swagger UI for authorization
     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     {
         Description = "JWT Authorization header using the Bearer scheme. Example: \"Bearer {token}\"",
@@ -40,32 +41,41 @@ builder.Services.AddSwaggerGen(c =>
             } });
 });
 builder.Services.TryAddSingleton<IHttpContextAccessor, HttpContextAccessor>();
-builder.Services.AddCors();
-builder.Services.AddControllers().AddNewtonsoftJson(); 
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin",
+        builder => builder.WithOrigins("http://chennaiexport-test.s3-website.ap-south-1.amazonaws.com")
+                          .AllowAnyHeader()
+                          .AllowAnyMethod());
+});
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+
 var app = builder.Build();
 
- 
+// Configure the HTTP request pipeline.
 
 app.UseHttpsRedirection();
 
 app.UseRouting();
+
 app.UseAuthentication();
 app.UseAuthorization();
-app.UseCors(opt => opt.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()); 
+
+// Use CORS policy
+app.UseCors("AllowSpecificOrigin");
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+
+if (app.Environment.IsDevelopment())
 {
-    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
-} 
+}
 
-app.MapGet("/", () => "Hello World!");
-
-var port = Environment.GetEnvironmentVariable("PORT") ?? "5049";
-app.Urls.Add($"http://localhost:{port}");
-
-app.Run(); 
+app.Run();
